1d36bebae8c7fae9296b13ea942c1ade97839c9f
Alex Pushkar <alexanderpushkar86@gmail.com>
2017-02-25 14:03:32 +0200

    Fill list using adapter.

--- app/src/main/java/com/example/apushkar/adapterviewtestapp/MainActivity.java
+++ app/src/main/java/com/example/apushkar/adapterviewtestapp/MainActivity.java
@@ -1,13 +1,105 @@
 package com.example.apushkar.adapterviewtestapp;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class FirstAdapter extends BaseAdapter {
+
+        final ArrayList<String> list;
+        final Context context;
+
+        FirstAdapter(ArrayList<String> list, Context context) {
+            this.list = list;
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            @SuppressLint("ViewHolder") View view = inflater.inflate(R.layout.list_item, parent, false);
+            TextView id = (TextView) view.findViewById(R.id.item_id);
+            TextView text = (TextView) view.findViewById(R.id.item_text);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS", Locale.US);
+            String currentDateAndTime = sdf.format(new Date());
+
+            id.setText(list.get(position));
+            text.setText(currentDateAndTime);
+
+            return view;
+        }
+    }
+
+    private ArrayList<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = new ArrayList<>();
+        final FirstAdapter adapter = new FirstAdapter(list, this);
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        Button add = (Button) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                list.add(String.valueOf(list.size()));
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        Button delete = (Button) findViewById(R.id.delete);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!list.isEmpty()) {
+                    list.remove(list.size() - 1);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        Button clear = (Button) findViewById(R.id.clear);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                list.clear();
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
